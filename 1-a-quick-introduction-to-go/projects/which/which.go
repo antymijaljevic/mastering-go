ef01c29b94f1d735f151d04ba0a75809b72aa21e
antymijaljevic <antymijaljevic@gmail.com>
2023-12-03 21:07:27 +0100

    data types

--- 1-a-quick-introduction-to-go/projects/which/which.go
+++ 1-a-quick-introduction-to-go/projects/which/which.go
@@ -13,22 +13,26 @@ func main() {
 		return
 	}
 
-	var file string = arguments[1]      // binary name
-	var path string = os.Getenv("PATH") // echo $PATH
-
+	// var file string = arguments[1]      // binary name
+	var path string = os.Getenv("PATH")   // echo $PATH
 	pathSplit := filepath.SplitList(path) // portable way of separating a list of paths ":"
-	for _, directory := range pathSplit {
-		fullPath := filepath.Join(directory, file) // concatenating the different parts of a path
-		// Does it exist?
-		fileInfo, err := os.Stat(fullPath)
-		if err == nil {
-			mode := fileInfo.Mode()
-			// Is it a regular file?
-			if mode.IsRegular() {
-				// Is it executable?
-				if mode&0111 != 0 { // unix permissions; mode & 0111 performs a bitwise AND operation between mode and 0111
-					fmt.Println(fullPath)
-					return
+
+	// search for multiple binaries
+	for _, v := range arguments[1:] {
+		for _, directory := range pathSplit {
+			fullPath := filepath.Join(directory, v) // concatenating the different parts of a path
+			// Does it exist?
+			fileInfo, err := os.Stat(fullPath)
+			if err == nil {
+				mode := fileInfo.Mode()
+				// Is it a regular file?
+				if mode.IsRegular() {
+					// Is it executable?
+					if mode&0111 != 0 { // unix permissions; mode & 0111 performs a bitwise AND operation between mode and 0111
+						fmt.Println(fullPath)
+						// check if there is more than one path for the same binary
+						continue
+					}
 				}
 			}
 		}
